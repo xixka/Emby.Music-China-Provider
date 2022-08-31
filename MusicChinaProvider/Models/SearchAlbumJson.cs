24e66a7e05e88069d8458a0b17be152f2f112a97
782856528@qq.com <weimengli521>
2022-08-31 11:45:04 +0800

    添加实体类

---
+++ MusicChinaProvider/Models/SearchAlbumJson.cs
@@ -0,0 +1,221 @@
+﻿using System.Collections.Generic;
+
+public class Artist
+{
+    /// <summary>
+    /// 周杰伦
+    /// </summary>
+    public string name { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int id { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int picId { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int img1v1Id { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string briefDesc { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string picUrl { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string img1v1Url { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int albumSize { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public List <string > @alias { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string trans { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int musicSize { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string picId_str { get; set; }
+}
+
+public class AlbumArtistsItem
+{
+    /// <summary>
+    /// 周杰伦
+    /// </summary>
+    public string name { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int id { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int picId { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int img1v1Id { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string briefDesc { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string picUrl { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string img1v1Url { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int albumSize { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public List <string > @alias { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string trans { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int musicSize { get; set; }
+}
+
+public class AlbumsItem
+{
+    /// <summary>
+    /// 说好不哭
+    /// </summary>
+    public string name { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int id { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string idStr { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string type { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int size { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int picId { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string blurPicUrl { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int companyId { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int pic { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string picUrl { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int publishTime { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string description { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string tags { get; set; }
+    /// <summary>
+    /// 杰威尔
+    /// </summary>
+    public string company { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string briefDesc { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public Artist artist { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public List <string > songs { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public List <string > @alias { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int status { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int copyrightId { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string commentThreadId { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public List <AlbumArtistsItem > artists { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string picId_str { get; set; }
+}
+
+public class AlbumResult
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public List <AlbumsItem > albums { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int albumCount { get; set; }
+}
+
+public class SearchAlbumJson
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public AlbumResult result { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int code { get; set; }
+}
--- MusicChinaProvider/Provider/MusicAlbumProvider.cs
+++ MusicChinaProvider/Provider/MusicAlbumProvider.cs
@@ -1,10 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using Emby.MeiamSub.Thunder.Consts;
 using MediaBrowser.Common.Net;
+using MediaBrowser.Controller.Configuration;
 using MediaBrowser.Controller.Entities.Audio;
 using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.IO;
+using MediaBrowser.Model.Logging;
 using MediaBrowser.Model.Providers;
+using MediaBrowser.Model.Serialization;
+using MusicProvider.Models;
 
 namespace MusicProvider
 {
@@ -15,6 +23,12 @@ namespace MusicProvider
         private const string UA = "PostmanRuntime/7.28.4";
         
         public string Name => "Music-China";
+        
+        private readonly ILogger _logger;
+        private readonly IJsonSerializer _json;
+        private readonly IHttpClient _httpClient;
+        private readonly IServerConfigurationManager _config;
+        private readonly IFileSystem _fileSystem;
         
         /**
          * 识别调用这里
@@ -29,9 +43,43 @@ namespace MusicProvider
          */
         public Task<MetadataResult<MusicAlbum>> GetMetadata(AlbumInfo info, CancellationToken cancellationToken)
         {
+            
             throw new System.NotImplementedException();
         }
-        
+
+        /**
+         * 根据专辑名字和歌手名字获取专辑ID
+         */
+        public async Task ByNameSearchAlbum(String Albumname,String MusicName)
+        {
+            String SearchKey = Albumname + " " + MusicName;
+            _logger.Debug($"搜索专辑 --> {SearchKey}");
+            SearchJson result;
+            var options = new HttpRequestOptions
+            {
+                Url =
+                    $"http://music.163.com/api/search/get/web?csrf_token=hlpretag=&s={SearchKey}&type=10&offset=0&limit=20",
+                UserAgent = UA,
+                EnableHttpCompression = false,
+                RequestHeaders = { { "X-Real-IP", Consts.RealIP } }
+            };
+            using (var json = await _httpClient.Get(options).ConfigureAwait(false))
+            {
+                using (var reader = new StreamReader(json))
+                {
+                    var jsonText = await reader.ReadToEndAsync().ConfigureAwait(false);
+                    _logger.Debug($"读取到网易云返回的结果 --> {jsonText}");
+                    result = _json.DeserializeFromString<SearchJson>(jsonText);
+                }
+            }
+            if (result != null && result.result.artistCount != 0)
+            {
+                // 有结果返回进行处理
+               
+            }
+            
+        }
+
         /**
          * 暂时不提供专辑图片
          */
